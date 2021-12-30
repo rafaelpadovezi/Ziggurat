2da72bf2ae99e018613c9c312570bffc9b6a380a
Rafael Miranda <rafaelpadovezi@gmail.com>
2021-12-30 17:29:46 -0300

    feature: mongodb example api with cap

---
+++ samples/Example.Cap.Mongo.Api/Models.cs
@@ -0,0 +1,8 @@
+﻿using Ziggurat;
+
+public class MyMessage : IMessage
+{
+    public string Text { get; set; }
+    public string MessageId { get; set; }
+    public string MessageGroup { get; set; }
+}
